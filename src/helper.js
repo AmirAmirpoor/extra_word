f3b967888a6c1d90aed044ef4bd936b9a2be9e5a
amir amirpoor <22155aamirpoor@gmail.com>
2021-07-08 14:09:10 +0430

    create helper file to help us feed the game board

---
+++ src/helper.js
@@ -0,0 +1,57 @@
+// words
+import WORDS from "./constants/words";
+
+// patterns
+import { fourWordsPatterns } from "./constants/patterns";
+import { fiveWordsPatterns } from "./constants/patterns";
+import { sixWordsPatterns } from "./constants/patterns";
+import { sevenWordsPatterns } from "./constants/patterns";
+
+const genRndBetween = (first, second) => {
+  const min = Math.min(first, second + 1);
+  const max = Math.max(first, second + 1);
+  const diff = max - min;
+
+  return Math.floor(Math.random() * diff) + min;
+};
+
+// THIS MIGHT NOT BE A GOOD FUNCTION NAME !!!
+export const feedTheBoard = () => {
+  const ALL_WORDS = [...WORDS];
+
+  const currentLevel = genRndBetween(5, 7); // 5, 6, 7
+
+  const secondStepWords = Array.from({ length: currentLevel }).map(() => {
+    const idx = Math.floor(Math.random() * ALL_WORDS.length);
+    return ALL_WORDS.splice(idx, 1)[0];
+  });
+
+  const patterns = {
+    5: {
+      firstStepPatterns: fourWordsPatterns[genRndBetween(0, 9)].patterns,
+      secondStepPatterns: fiveWordsPatterns[genRndBetween(0, 9)].patterns,
+    },
+
+    6: {
+      firstStepPatterns: fiveWordsPatterns[genRndBetween(0, 9)].patterns,
+      secondStepPatterns: sixWordsPatterns[genRndBetween(0, 9)].patterns,
+    },
+    7: {
+      firstStepPatterns: sixWordsPatterns[genRndBetween(0, 9)].patterns,
+      secondStepPatterns: sevenWordsPatterns[genRndBetween(0, 9)].patterns,
+    },
+  };
+
+  const len = secondStepWords.length - 1;
+  const extraWord = secondStepWords[genRndBetween(0, len)];
+
+  return {
+    firstStepWords: secondStepWords.filter((word) => word !== extraWord),
+    firstStepPatterns: patterns[currentLevel].firstStepPatterns,
+
+    secondStepWords,
+    secondStepPatterns: patterns[currentLevel].secondStepPatterns,
+
+    extraWord,
+  };
+};
