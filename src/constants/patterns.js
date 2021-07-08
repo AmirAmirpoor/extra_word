234379858c53fdb0d3eb872df632e7da53592232
amir amirpoor <22155aamirpoor@gmail.com>
2021-07-08 12:46:05 +0430

    add words and different board patterns

---
+++ src/constants/patterns.js
@@ -0,0 +1,1024 @@
+export const fourWordsPatterns = [
+  {
+    id: 0,
+    patterns: [
+      {
+        gridColumn: "span 2",
+        gridRow: "span 2",
+      },
+      {
+        gridColumn: "span 2",
+      },
+      {
+        gridColumn: "span 2",
+        gridRow: "span 3",
+      },
+      {
+        gridColumn: "span 2",
+        gridRow: "4 / span 1",
+      },
+    ],
+  },
+  {
+    id: 1,
+    patterns: [
+      {
+        gridColumn: "span 2",
+        gridRow: "span 2",
+      },
+      {
+        gridColumn: "span 2",
+      },
+      {
+        gridColumn: "span 2",
+        gridRow: "span 2",
+      },
+      {
+        gridColumn: "span 2",
+        gridRow: "span 2",
+      },
+    ],
+  },
+  {
+    id: 2,
+    patterns: [
+      {
+        gridColumn: "span 3",
+      },
+      {
+        gridColumn: "span 2",
+      },
+      {
+        gridColumn: "span 2",
+        gridRow: "span 3",
+      },
+      {
+        gridColumn: "span 2",
+        gridRow: "span 2",
+      },
+    ],
+  },
+  {
+    id: 3,
+    patterns: [
+      {
+        gridColumn: "span 2",
+      },
+      {
+        gridColumn: "span 2",
+        gridRow: "span 3",
+      },
+      {
+        gridColumn: "span 2",
+        gridRow: "span 2",
+      },
+      {
+        gridColumn: "2 / span 3",
+      },
+    ],
+  },
+  {
+    id: 4,
+    patterns: [
+      {
+        gridColumn: "span 2",
+        gridRow: "span 3",
+      },
+      {
+        gridColumn: "span 2",
+        gridRow: "span 2",
+      },
+      {
+        gridColumn: "span 2",
+      },
+      {
+        gridColumn: "span 3",
+      },
+    ],
+  },
+  {
+    id: 5,
+    patterns: [
+      {
+        gridColumn: "2 / span 3",
+      },
+      {
+        gridColumn: "span 2",
+        gridRow: "span 2",
+      },
+      {
+        gridColumn: "span 2",
+      },
+      {
+        gridColumn: "2 / span 3",
+      },
+    ],
+  },
+  {
+    id: 6,
+    patterns: [
+      {
+        gridColumn: "2 / span 3",
+      },
+      {
+        gridColumn: "span 4",
+      },
+      {
+        gridColumn: "span 2",
+      },
+      {
+        gridColumn: "span 2",
+        gridRow: "span 2",
+      },
+    ],
+  },
+  {
+    id: 7,
+    patterns: [
+      {
+        gridColumn: "span 4",
+      },
+      {
+        gridColumn: "span 2",
+        gridRow: "span 2",
+      },
+      {
+        gridColumn: "span 2",
+        gridRow: "span 3",
+      },
+      {
+        gridColumn: "span 2",
+      },
+    ],
+  },
+  {
+    id: 8,
+    patterns: [
+      {
+        gridColumn: "span 2",
+      },
+      {
+        gridColumn: "span 2",
+        gridRow: "span 3",
+      },
+      {
+        gridColumn: "span 2",
+        gridRow: "span 3",
+      },
+      {
+        gridColumn: "span 2",
+      },
+    ],
+  },
+  {
+    id: 9,
+    patterns: [
+      {
+        gridColumn: "span 2",
+        gridRow: "span 2",
+      },
+      {
+        gridColumn: "span 2",
+        gridRow: "span 2",
+      },
+      {
+        gridColumn: "span 2",
+        gridRow: "span 2",
+      },
+      {
+        gridColumn: "span 2",
+        gridRow: "span 2",
+      },
+    ],
+  },
+];
+
+export const fiveWordsPatterns = [
+  {
+    id: 0,
+    patterns: [
+      {
+        gridColumn: "span 2",
+        gridRow: "span 2",
+      },
+      {
+        gridColumn: "span 3",
+        gridRow: "span 2",
+      },
+      {
+        gridColumn: "span 3",
+      },
+      {
+        gridColumn: "span 2",
+        gridRow: "span 3",
+      },
+      {
+        gridColumn: "span 3",
+        gridRow: "span 2",
+      },
+    ],
+  },
+  {
+    id: 1,
+    patterns: [
+      {
+        gridColumn: "span 2",
+        gridRow: "span 3",
+      },
+      {
+        gridColumn: "span 3",
+        gridRow: "span 2",
+      },
+      {
+        gridColumn: "4 / span 2",
+        gridRow: "span 2",
+      },
+      {
+        gridColumn: "span 3",
+        gridRow: "span 2",
+      },
+      {
+        gridColumn: "span 2",
+      },
+    ],
+  },
+  {
+    id: 2,
+    patterns: [
+      {
+        gridColumn: "span 3",
+      },
+      {
+        gridColumn: "span 2",
+        gridRow: "span 3",
+      },
+      {
+        gridColumn: "span 2",
+        gridRow: "span 2",
+      },
+      {
+        gridColumn: "span 3",
+        gridRow: "span 2",
+      },
+      {
+        gridColumn: "span 2",
+        gridRow: "span 2",
+      },
+    ],
+  },
+  {
+    id: 3,
+    patterns: [
+      {
+        gridColumn: "span 3",
+      },
+      {
+        gridColumn: "span 2",
+        gridRow: "span 2",
+      },
+      {
+        gridColumn: "span 2",
+        gridRow: "span 2",
+      },
+      {
+        gridColumn: "span 3",
+      },
+      {
+        gridColumn: "span 4",
+        gridRow: "span 2",
+      },
+    ],
+  },
+  {
+    id: 4,
+    patterns: [
+      { gridColumnStart: 3, gridColumnEnd: 6 },
+      {
+        gridColumn: "span 2",
+        gridRow: "span 3",
+      },
+      {
+        gridColumn: "span 3",
+        gridRow: "span 2",
+      },
+      {
+        gridColumnStart: 4,
+        gridColumnEnd: 6,
+        gridRow: "span 2",
+      },
+      {
+        gridColumn: "span 3",
+      },
+    ],
+  },
+  {
+    id: 5,
+    patterns: [
+      {
+        gridColumn: "span 2",
+        gridRow: "span 2",
+      },
+      {
+        gridColumn: "span 3",
+      },
+      {
+        gridColumn: "span 2",
+        gridRow: "span 3",
+      },
+      {
+        gridColumn: "span 2",
+        gridRow: "span 3",
+      },
+      {
+        gridColumn: "span 3",
+      },
+    ],
+  },
+  {
+    id: 6,
+    patterns: [
+      {
+        gridColumn: "span 3",
+      },
+      {
+        gridColumn: "span 2",
+        gridRow: "span 2",
+      },
+      {
+        gridColumn: "span 3",
+        gridRow: "span 2",
+      },
+      {
+        gridColumn: "span 2",
+        gridRow: "span 2",
+      },
+      {
+        gridColumnStart: 1,
+        gridColumnEnd: 4,
+        gridRow: "span 2",
+      },
+    ],
+  },
+  {
+    id: 7,
+    patterns: [
+      {
+        gridColumn: "span 3",
+        gridRow: "span 2",
+      },
+      {
+        gridColumnStart: 4,
+        gridColumnEnd: 6,
+        gridRowStart: 2,
+        gridRowEnd: 3,
+      },
+      {
+        gridColumn: "span 2",
+        gridRow: "span 2",
+      },
+      {
+        gridColumn: "span 2",
+        gridRow: "span 3",
+      },
+      {
+        gridColumn: "span 2",
+      },
+    ],
+  },
+  {
+    id: 8,
+    patterns: [
+      {
+        gridColumn: "span 2",
+        gridRow: "span 2",
+      },
+      {
+        gridColumn: "span 3",
+        gridRow: "span 2",
+      },
+      {
+        gridColumn: "span 3",
+        gridRow: "span 2",
+      },
+      {
+        gridColumn: "span 2",
+        gridRow: "span 2",
+      },
+      {
+        gridColumn: "span 2",
+      },
+      {
+        gridColumn: "span 3",
+      },
+    ],
+  },
+  {
+    id: 9,
+    patterns: [
+      {
+        gridColumn: "span 3",
+      },
+      {
+        gridColumn: "span 2",
+        gridRow: "span 2",
+      },
+      {
+        gridColumn: "span 3",
+      },
+      {
+        gridColumn: "span 3",
+        gridRow: "span 3",
+      },
+      {
+        gridColumn: "span 2",
+        gridRow: "span 3",
+      },
+    ],
+  },
+];
+
+export const sixWordsPatterns = [
+  {
+    id: 0,
+    patterns: [
+      {
+        gridColumn: "span 3",
+        gridRow: "span 2",
+      },
+      {
+        gridColumn: "span 3",
+      },
+      {
+        gridColumn: "span 3",
+        gridRow: "span 3",
+      },
+      {
+        gridColumn: "span 3",
+        gridRow: "span 3",
+      },
+      {
+        gridColumn: "span 3",
+        gridRow: "span 2",
+      },
+      {
+        gridColumn: "span 2",
+      },
+    ],
+  },
+  {
+    id: 1,
+    patterns: [
+      {
+        gridColumn: "span 2",
+        gridRow: "span 3",
+      },
+      {
+        gridColumn: "span 4",
+      },
+
+      {
+        gridColumnStart: 4,
+        gridColumnEnd: 7,
+        gridRow: "span 2",
+      },
+      {
+        gridColumn: "span 4",
+        gridRow: "span 2",
+      },
+      {
+        gridColumnStart: 5,
+        gridColumnEnd: 7,
+        gridRow: "span 3",
+      },
+      {
+        gridColumn: "span 4",
+      },
+    ],
+  },
+  {
+    id: 2,
+    patterns: [
+      {
+        gridColumn: "span 3",
+      },
+      {
+        gridColumn: "span 2",
+        gridRow: "span 4",
+      },
+      {
+        gridColumn: "span 3",
+      },
+      {
+        gridColumn: "span 3",
+      },
+      {
+        gridColumn: "span 3",
+        gridRow: "span 3",
+      },
+      {
+        gridColumn: "span 3",
+        gridRow: "span 2",
+      },
+    ],
+  },
+  {
+    id: 3,
+    patterns: [
+      {
+        gridColumn: "span 2",
+        gridRow: "span 4",
+      },
+      {
+        gridColumnStart: 4,
+        gridColumnEnd: 7,
+        gridRow: "span 2",
+      },
+      {
+        gridColumn: "span 3",
+        gridRow: "span 2",
+      },
+      {
+        gridColumn: "span 3",
+      },
+      {
+        gridColumn: "span 3",
+        gridRow: "span 2",
+      },
+      {
+        gridColumn: "span 3",
+      },
+    ],
+  },
+  {
+    id: 4,
+    patterns: [
+      {
+        gridColumnStart: 2,
+        gridColumnEnd: 4,
+        gridRow: "span 2",
+      },
+      {
+        gridColumn: "span 3",
+        gridRow: "span 2",
+      },
+      {
+        gridColumn: "span 2",
+        gridRow: "span 3",
+      },
+      {
+        gridColumn: "span 4",
+      },
+      {
+        gridColumn: "span 3",
+      },
+      {
+        gridColumn: "span 4",
+        gridRow: "span 2",
+      },
+    ],
+  },
+  {
+    id: 5,
+    patterns: [
+      {
+        gridColumnStart: 2,
+        gridColumnEnd: 4,
+        gridRow: "span 2",
+      },
+      {
+        gridColumn: "span 3",
+        gridRow: "span 3",
+      },
+      {
+        gridColumn: "span 3",
+      },
+      {
+        gridColumnStart: 3,
+        gridColumnEnd: 5,
+        gridRow: "span 3",
+      },
+      {
+        gridColumn: "span 2",
+        gridRow: "span 2",
+      },
+      {
+        gridColumn: "span 2",
+        gridRow: "span 2",
+      },
+    ],
+  },
+  {
+    id: 6,
+    patterns: [
+      {
+        gridColumnStart: 3,
+        gridColumnEnd: 7,
+      },
+      {
+        gridColumn: "span 4",
+      },
+      {
+        gridColumn: "span 2",
+      },
+      {
+        gridColumn: "span 2",
+      },
+      {
+        gridColumn: "span 4",
+        gridRow: "span 4",
+      },
+      {
+        gridColumn: "span 2",
+        gridRow: "span 3",
+      },
+    ],
+  },
+  {
+    id: 7,
+    patterns: [
+      {
+        gridColumn: "span 6",
+      },
+      {
+        gridColumn: "span 2",
+        gridRow: "span 5",
+      },
+      {
+        gridColumn: "span 2",
+        gridRow: "span 2",
+      },
+      {
+        gridColumn: "span 2",
+        gridRow: "span 3",
+      },
+      {
+        gridColumn: "span 2",
+        gridRow: "span 3",
+      },
+      {
+        gridColumn: "span 2",
+        gridRow: "span 2",
+      },
+    ],
+  },
+  {
+    id: 8,
+    patterns: [
+      {
+        gridColumn: "span 4",
+      },
+      {
+        gridColumn: "span 2",
+        gridRow: "span 5",
+      },
+      {
+        gridColumn: "span 2",
+        gridRow: "span 3",
+      },
+      {
+        gridColumn: "span 2",
+        gridRow: "span 3",
+      },
+      {
+        gridColumn: "span 4",
+        gridRow: "span 2",
+      },
+      {
+        gridColumn: "span 2",
+      },
+    ],
+  },
+  {
+    id: 9,
+    patterns: [
+      {
+        gridColumn: "span 3",
+        gridRow: "span 4",
+      },
+      {
+        gridColumn: "span 3",
+        gridRow: "span 2",
+      },
+      {
+        gridColumn: "span 3",
+        gridRow: "span 2",
+      },
+      {
+        gridColumn: "span 3",
+      },
+      {
+        gridColumn: "span 3",
+        gridRow: "span 2",
+      },
+      {
+        gridColumn: "span 3",
+      },
+    ],
+  },
+];
+
+export const sevenWordsPatterns = [
+  {
+    id: 0,
+    patterns: [
+      {
+        gridColumn: "span 5",
+        gridRow: "span 2",
+      },
+      {
+        gridColumn: "span 2",
+        gridRow: "span 5",
+      },
+      {
+        gridColumn: "span 2",
+        gridRow: "span 4",
+      },
+      {
+        gridColumn: "span 3",
+        gridRow: "span 3",
+      },
+      {
+        gridColumn: "span 4",
+      },
+      {
+        gridColumn: "span 3",
+      },
+      {
+        gridColumn: "span 3",
+      },
+    ],
+  },
+  {
+    id: 1,
+    patterns: [
+      {
+        gridColumn: "span 2",
+        gridRow: "span 3",
+      },
+      {
+        gridColumn: "span 5",
+        gridRow: "span 2",
+      },
+      {
+        gridColumn: "span 5",
+      },
+      {
+        gridColumn: "span 3",
+        gridRow: "span 3",
+      },
+      {
+        gridColumn: "span 2",
+        gridRow: "span 4",
+      },
+      {
+        gridColumn: "span 2",
+        gridRow: "span 3",
+      },
+      {
+        gridColumn: "span 3",
+      },
+    ],
+  },
+  {
+    id: 2,
+    patterns: [
+      {
+        gridColumn: "span 3",
+        gridRow: "span 2",
+      },
+      {
+        gridColumn: "span 4",
+        gridRow: "span 2",
+      },
+      {
+        gridColumn: "span 3",
+        gridRow: "span 4",
+      },
+      {
+        gridColumn: "span 3",
+        gridRow: "span 2",
+      },
+      {
+        gridColumn: "span 3",
+        gridRow: "span 2",
+      },
+      {
+        gridColumn: "span 4",
+      },
+      {
+        gridColumn: "span 3",
+      },
+    ],
+  },
+  {
+    id: 3,
+    patterns: [
+      {
+        gridColumn: "span 2",
+        gridRow: "span 5",
+      },
+      {
+        gridColumn: "span 5",
+      },
+      {
+        gridColumn: "span 4",
+        gridRow: "span 2",
+      },
+      {
+        gridColumn: "span 3",
+        gridRow: "span 3",
+      },
+      {
+        gridColumn: "span 2",
+        gridRow: "span 3",
+      },
+      {
+        gridColumn: "span 2",
+        gridRow: "span 2",
+      },
+      {
+        gridColumn: "span 5",
+      },
+    ],
+  },
+  {
+    id: 4,
+    patterns: [
+      {
+        gridColumn: "4 / span 4",
+      },
+      {
+        gridColumn: "span 3",
+      },
+      {
+        gridColumn: "span 2",
+        gridRow: "span 4",
+      },
+      {
+        gridColumn: "span 2",
+        gridRow: "span 4",
+      },
+      {
+        gridColumn: "span 3",
+        gridRow: "span 3",
+      },
+      {
+        gridColumn: "span 5",
+        gridRow: "span 2",
+      },
+      {
+        gridColumn: "span 2",
+        gridRow: "span 2",
+      },
+    ],
+  },
+  {
+    id: 5,
+    patterns: [
+      {
+        gridColumn: "span 3",
+        gridRow: "span 3",
+      },
+      {
+        gridColumn: "span 4",
+        gridRow: "span 2",
+      },
+      {
+        gridColumn: "span 4",
+      },
+      {
+        gridColumn: "span 4",
+        gridRow: "span 2",
+      },
+      {
+        gridColumn: "span 3",
+        gridRow: "span 3",
+      },
+      {
+        gridColumn: "span 4",
+      },
+      {
+        gridColumn: "span 4",
+      },
+    ],
+  },
+  {
+    id: 6,
+    patterns: [
+      {
+        gridColumn: "span 6",
+      },
+      {
+        gridColumn: "span 4",
+        gridRow: "span 2",
+      },
+      {
+        gridColumn: "span 3",
+        gridRow: "span 3",
+      },
+      {
+        gridColumn: "span 4",
+      },
+      {
+        gridColumn: "span 4",
+        gridRow: "span 2",
+      },
+      {
+        gridColumn: "span 3",
+        gridRow: "span 2",
+      },
+      {
+        gridColumn: "span 5",
+      },
+    ],
+  },
+  {
+    id: 7,
+    patterns: [
+      {
+        gridColumn: "span 3",
+        gridRow: "span 4",
+      },
+      {
+        gridColumn: "span 4",
+      },
+      {
+        gridColumn: "span 4",
+      },
+      {
+        gridColumn: "span 4",
+        gridRow: "span 3",
+      },
+      {
+        gridColumn: "span 3",
+      },
+      {
+        gridColumn: "span 5",
+      },
+      {
+        gridColumn: "span 6",
+      },
+    ],
+  },
+  {
+    id: 8,
+    patterns: [
+      {
+        gridColumn: "span 2",
+        gridRow: "span 4",
+      },
+      {
+        gridColumn: "span 5",
+      },
+      {
+        gridColumn: "span 3",
+        gridRow: "span 3",
+      },
+      {
+        gridColumn: "span 2",
+        gridRow: "span 5",
+      },
+      {
+        gridColumn: "span 5",
+        gridRow: "span 2",
+      },
+      {
+        gridColumn: "span 5",
+      },
+      {
+        gridColumn: "span 2",
+      },
+    ],
+  },
+  {
+    id: 9,
+    patterns: [
+      {
+        gridColumn: "span 2",
+        gridRow: "span 3",
+      },
+      {
+        gridColumn: "span 3",
+        gridRow: "span 3",
+      },
+      {
+        gridColumn: "span 2",
+        gridRow: "span 3",
+      },
+      {
+        gridColumn: "span 7",
+        gridRow: "span 2",
+      },
+      {
+        gridColumn: "span 2",
+        gridRow: "span 2",
+      },
+      {
+        gridColumn: "span 5",
+      },
+      {
+        gridColumn: "span 5",
+      },
+    ],
+  },
+];
