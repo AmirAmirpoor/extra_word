bd22cd2f617153533d36677da763d9ff2f6a1ded
amir amirpoor <22155aamirpoor@gmail.com>
2021-07-09 20:27:47 +0430

    use HashRouter instead of BrowserRouter

--- src/index.js
+++ src/index.js
@@ -4,11 +4,11 @@ import ReactDOM from "react-dom";
 import App from "./App";
 import reportWebVitals from "./reportWebVitals";
 
-import { BrowserRouter as Router } from "react-router-dom";
+import { HashRouter as Router } from "react-router-dom";
 
 ReactDOM.render(
   <React.StrictMode>
-    <Router basename={process.env.PUBLIC_URL}>
+    <Router>
       <App />
     </Router>
   </React.StrictMode>,
